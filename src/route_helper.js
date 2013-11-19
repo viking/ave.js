9462e1aa79930de11e16151ec1c920125e3f62c1
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-11-19 09:49:41 -0600

    Add RouteHelper.go()

--- src/route_helper.js
+++ src/route_helper.js
@@ -8,7 +8,15 @@ ave.RouteHelper.prototype.urlFor = function(url) {
   return url;
 };
 
+ave.RouteHelper.prototype.go = function(url) {
+  if (this.parentNode) {
+    return this.parentNode.go(url);
+  }
+  return url;
+};
+
 ave.RouteHelper.mixin = function(obj) {
   obj.urlFor = ave.RouteHelper.prototype.urlFor;
+  obj.go = ave.RouteHelper.prototype.go;
   ave.RouteHelper.call(obj);
 };
