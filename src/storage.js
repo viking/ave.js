2e891f4680c753c7d9841d18a491c7a76f77d28a
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-01-13 13:37:12 -0600

    Add remote storage backend support
    - add Storage#getMode(), which returns 'local' or 'remote'
    - add callback parameter to Storage#register()
    - get/post JSON from remote server
    - add Blob polyfill for testing

--- src/storage.js
+++ src/storage.js
@@ -7,25 +7,68 @@ maria.Model.subclass(ave, 'Storage', {
   properties: {
     getBackend: function() {
       if (typeof(this._backend) == "undefined") {
-        this._backend = localStorage;
+        this.setBackend(localStorage);
       }
       return this._backend;
     },
 
     setBackend: function(backend) {
       this._backend = backend;
+      if (typeof(backend) == "object") {
+        this._mode = "local";
+      }
+      else if (typeof(backend) == "string") {
+        this._mode = "remote";
+      }
     },
 
-    register: function(collectionName, setModelConstructor) {
-      var backend = this.getBackend();
-      var setModel;
-      if (collectionName in backend) {
-        setModel = setModelConstructor.fromJSON(backend[collectionName]);
-      }
-      else {
-        setModel = new setModelConstructor();
-      }
+    getMode: function() {
+      return this._mode;
+    },
 
+    register: function(collectionName, setModelConstructor, callback) {
+      var backend = this.getBackend();
+      var mode = this.getMode();
+
+      if (mode == "local") {
+        var setModel;
+
+        if (collectionName in backend) {
+          setModel = setModelConstructor.fromJSON(backend[collectionName]);
+        }
+        else {
+          setModel = new setModelConstructor();
+        }
+
+        this._register(collectionName, setModelConstructor, setModel);
+      }
+      else if (mode == "remote") {
+        var xhr = new XMLHttpRequest();
+        var self = this;
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState != 4)
+            return;
+
+          var setModel;
+          if (xhr.responseText != '') {
+            setModel = setModelConstructor.fromJSON(xhr.responseText);
+          }
+          else {
+            setModel = new setModelConstructor();
+          }
+          self._register(collectionName, setModelConstructor, setModel);
+          callback();
+        };
+        xhr.open("get", backend + "/" + collectionName + ".json");
+        xhr.send();
+      }
+    },
+
+    getCollection: function(collectionName) {
+      return this._collections[collectionName].setModel;
+    },
+
+    _register: function(collectionName, setModelConstructor, setModel) {
       var self = this;
       maria.on(setModel, 'change', function(evt) {
         self._update(collectionName);
@@ -37,16 +80,28 @@ maria.Model.subclass(ave, 'Storage', {
       };
     },
 
-    getCollection: function(collectionName) {
-      return this._collections[collectionName].setModel;
-    },
-
     _update: function(collectionName) {
-      var setModel = this._collections[collectionName].setModel;
       var backend = this.getBackend();
-      backend[collectionName] = setModel.toJSON();
+      var mode = this.getMode();
+      var data = this._collections[collectionName].setModel.toJSON();
+      var event = {type: 'change', collectionName: collectionName};
 
-      this.dispatchEvent({type: 'change', collectionName: collectionName});
-    }
+      if (mode == "local") {
+        backend[collectionName] = data;
+        this.dispatchEvent(event);
+      }
+      else if (mode == "remote") {
+        var xhr = new XMLHttpRequest();
+        var self = this;
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState != 4)
+            return;
+
+          self.dispatchEvent(event);
+        };
+        xhr.open("post", backend + "/" + collectionName + ".json");
+        xhr.send(new Blob([data]));
+      }
+    },
   }
 });
--- test/js/test_storage.js
+++ test/js/test_storage.js
@@ -17,50 +17,113 @@ define([
   }
 
   return new prod.Suite('Storage', {
-    setUp: function() {
-      this.backend = {};
-      this.store = new ave.Storage();
-      this.store.setBackend(this.backend);
-    },
+    "local": new prod.Suite('local', {
+      setUp: function() {
+        this.backend = {};
+        this.store = new ave.Storage();
+        this.store.setBackend(this.backend);
+      },
 
-    "adding to an registered collection": function() {
-      var modelClass = newModelClass({
-        attributeNames: ['id', 'name']
-      });
-      var setModelClass = newSetModelClass({
-        modelConstructor: modelClass
-      });
+      "adding to an registered collection": function() {
+        var modelClass = newModelClass({
+          attributeNames: ['id', 'name']
+        });
+        var setModelClass = newSetModelClass({
+          modelConstructor: modelClass
+        });
 
-      this.store.register("foos", setModelClass);
-      var setModel = this.store.getCollection("foos");
+        this.store.register("foos", setModelClass);
+        var setModel = this.store.getCollection("foos");
 
-      var spy = sinon.spy();
-      maria.on(this.store, 'change', spy);
+        var spy = sinon.spy();
+        maria.on(this.store, 'change', spy);
 
-      var model = new modelClass();
-      model.setName('foo');
-      sinon.stub(setModel, 'toJSON').returns('foobar');
-      setModel.add(model);
+        var model = new modelClass();
+        model.setName('foo');
+        sinon.stub(setModel, 'toJSON').returns('foobar');
+        setModel.add(model);
 
-      this.assertEquals('foobar', this.backend['foos']);
-      this.assertCalled(spy);
+        this.assertEquals('foobar', this.backend['foos']);
+        this.assertCalled(spy);
 
-      var evt = spy.getCall(0).args[0];
-      this.assertEquals('foos', evt.collectionName);
-    },
+        var evt = spy.getCall(0).args[0];
+        this.assertEquals('foos', evt.collectionName);
+      },
 
-    "registering collection with saved data": function() {
-      var modelClass = newModelClass({
-        attributeNames: ['id', 'name']
-      });
-      var setModelClass = newSetModelClass({
-        modelConstructor: modelClass
-      });
-      this.backend["foos"] = '{"models":[{"id":1,"name":"foo"}],"_nextId":2}';
-      this.store.register("foos", setModelClass);
+      "registering collection with saved data": function() {
+        var modelClass = newModelClass({
+          attributeNames: ['id', 'name']
+        });
+        var setModelClass = newSetModelClass({
+          modelConstructor: modelClass
+        });
+        this.backend["foos"] = '{"models":[{"id":1,"name":"foo"}],"_nextId":2}';
+        this.store.register("foos", setModelClass);
 
-      var setModel = this.store.getCollection("foos");
-      this.assertEquals(1, setModel.size);
-    }
+        var setModel = this.store.getCollection("foos");
+        this.assertEquals(1, setModel.size);
+      }
+    }),
+
+    "remote": new prod.Suite('remote', {
+      setUp: function() {
+        this.store = new ave.Storage();
+        this.store.setBackend("http://example.com");
+        this.server = sinon.fakeServer.create();
+      },
+
+      tearDown: function() {
+        this.server.restore();
+      },
+
+      "adding to an registered collection": function(done) {
+        var modelClass = newModelClass({
+          attributeNames: ['id', 'name']
+        });
+        var setModelClass = newSetModelClass({
+          modelConstructor: modelClass
+        });
+
+        this.server.respondWith("GET", "http://example.com/foos.json",
+          [ 200, { "Content-Type": "application/json" }, '' ]
+        );
+        var self = this;
+        this.store.register("foos", setModelClass, function() {
+          var setModel = self.store.getCollection("foos");
+
+          var called = false;
+          self.server.respondWith("POST", "http://example.com/foos.json", function(request) {
+            called = true;
+            request.respond(200, {}, "");
+          });
+          maria.on(self.store, 'change', done(function(evt) {
+            self.assert(called);
+            self.assertEquals('foos', evt.collectionName);
+          }));
+
+          var model = new modelClass();
+          model.setName('foo');
+          sinon.stub(setModel, 'toJSON').returns('foobar');
+          setModel.add(model);
+        });
+        this.server.respond();
+      },
+
+      /*
+      "registering collection with saved data": function() {
+        var modelClass = newModelClass({
+          attributeNames: ['id', 'name']
+        });
+        var setModelClass = newSetModelClass({
+          modelConstructor: modelClass
+        });
+        this.backend["foos"] = '{"models":[{"id":1,"name":"foo"}],"_nextId":2}';
+        this.store.register("foos", setModelClass);
+
+        var setModel = this.store.getCollection("foos");
+        this.assertEquals(1, setModel.size);
+      }
+      */
+    })
   });
 });
